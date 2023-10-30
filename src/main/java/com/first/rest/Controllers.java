b46a5d676c7168bd51faba60bbe32454c0d9d22a
kiran-tech123 <132247954+kiran-tech123@users.noreply.github.com>
2023-10-30 18:52:39 -0400

    Update Controllers.java
    added import sample

--- src/main/java/com/first/rest/Controllers.java
+++ src/main/java/com/first/rest/Controllers.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import 
 @RestController
 public class Controllers {
 
