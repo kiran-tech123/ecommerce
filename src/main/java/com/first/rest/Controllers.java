1acfc4deef2ff9425e4fdf83b87e0be8da574e2a
kiran-tech123 <132247954+kiran-tech123@users.noreply.github.com>
2023-10-30 18:56:18 -0400

    Update Controllers.java
    new changes 2 import

--- src/main/java/com/first/rest/Controllers.java
+++ src/main/java/com/first/rest/Controllers.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 @RestController
 public class Controllers {
 
