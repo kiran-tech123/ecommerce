ae58b01e6573be91ed272c58cc1360a49acf165f
kiran-tech123 <132247954+kiran-tech123@users.noreply.github.com>
2023-10-30 18:55:30 -0400

    Update Controllers.java
    direct merge

--- src/main/java/com/first/rest/Controllers.java
+++ src/main/java/com/first/rest/Controllers.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
 @RestController
 public class Controllers {
 
